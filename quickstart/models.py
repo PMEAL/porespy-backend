2740eb163faca07c810179e600a882cbaab4dace
espitiaandres <espitiaandres123@gmail.com>
2020-12-21 18:09:52 -0500

    updated generated_image property to match README.md

--- quickstart/models.py
+++ quickstart/models.py
@@ -5,6 +5,8 @@ import porespy as ps
 from io import BytesIO
 from PIL import Image
 import numpy as np
+from skimage import data
+from skimage.filters import threshold_otsu
 import matplotlib.pyplot as plt
 
 # IMPORTANT: Run these 3 commands when creating a new model
@@ -36,13 +38,24 @@ class PoreSpyTutorial(models.Model):
         float_porosity = float(self.porosity)
         int_blobiness = int(self.blobiness)
         shape_array = [int_dimension_x, int_dimension_y]
-
         im = ps.generators.blobs(shape=shape_array, porosity=float_porosity, blobiness=int_blobiness).tolist()
-        pil_img = Image.fromarray(np.array(im))
+        im_data = np.array(im)
+        pil_img = Image.fromarray(im_data).convert("RGB")
         buff = BytesIO()
+
+        for x in range(int_dimension_x):
+            for y in range(int_dimension_y):
+                black = (0, 0, 0)
+                white = (255, 255, 255)
+                yellow = (255, 255, 0)
+                purple = (128, 0, 128)
+                coordinate = x, y
+                if pil_img.getpixel(coordinate) == black:
+                    pil_img.putpixel((x, y), purple)
+                else:
+                    pil_img.putpixel((x, y), yellow)
+
         pil_img.save(buff, format="PNG")
         new_im_string = base64.b64encode(buff.getvalue()).decode("utf-8")
         return new_im_string
-
-
 
